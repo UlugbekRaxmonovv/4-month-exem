830715e5af56ff24ce3747d274fe014b485cd84a
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-02-19 23:18:02 +0500

    dark mork

--- main.js
+++ main.js
@@ -3,13 +3,27 @@ let burger = document.getElementById("burger_menu");
 burger.addEventListener("click", function () {
   document.body.classList.toggle("open_menu");
 });
-// let modeBtn = document.getElementById("dark-light");
-
-// modeBtn.addEventListener("click", function () {
-//   document.body.classList.toggle("dark");
-// });
 
 let modeBtn = document.getElementById("light-dark");
+
 modeBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 });
+
+
+window.addEventListener('scroll', function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById('backtop');
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = '20px';
+  } else {
+    backtop.style.bottom = '-50px';
+  }
+}
