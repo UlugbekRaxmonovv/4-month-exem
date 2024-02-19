42b860804fff5eb4488de3091933d3c01b7ac629
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-02-19 20:18:40 +0500

    Exem

---
+++ main.js
@@ -0,0 +1,15 @@
+let burger = document.getElementById("burger_menu");
+
+burger.addEventListener("click", function () {
+  document.body.classList.toggle("open_menu");
+});
+// let modeBtn = document.getElementById("dark-light");
+
+// modeBtn.addEventListener("click", function () {
+//   document.body.classList.toggle("dark");
+// });
+
+let modeBtn = document.getElementById("light-dark");
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
